aff4450064291c1e81a41e6a69bb99b330099146
SanchitaSubramaniam <86506500+SanchitaSubramaniam@users.noreply.github.com>
2021-11-23 15:34:01 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-var updatedImage = [
+var updatedImages = [
   "Cartoon 8 Year Old Daughter.jpg", "Cartoon 12 Year Old Daughter.jpg", "Cartoon Dad.jpg", "Cartoon Mom.jpg"
 ];
+
+var updatedNames = [
+  "Tanusha Subramaniam", "Sanchita Subramaniam", "Prasad Subramaniam", "Vidya Sekhar"
+]
 var i = 0;
+
 function update()
 {
-    i++;
-    var numbers_of_family_member_in_array = 5
+    var numbers_of_family_member_in_array = 3
     if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
-      }
+    {
+        i = 0;
+    }
     
-      var updatedImage = {images};
-      var updatedName = {names};
+    var updatedImage = updatedImages[i];
+    var updatedName = updatedNames[i];
  
     document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
+    i++;
 }
